e4ccc675ff517c33b79821990b996745fc6aa8da
Graham Neubig <neubig@gmail.com>
2014-06-09 13:01:17 +0900

    Added exercise 1

---
+++ download/01-unigramlm/sample-code/00-integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+first_int = 1
+second_int = 2
+
+print first_int/second_int
+
+print float(first_int)/second_int
---
+++ download/01-unigramlm/sample-code/01-log.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import math
+
+print math.log(100)     # ln(100)
+print math.log(100, 10) # log10(100)
